af68645eae1e355d0b73e5508b448c4f7a5f91b6
Ilya <ilf@bk.ru>
2019-07-15 12:34:29 +0300

    Small changes

--- data/dialogues/character/create/index.js
+++ data/dialogues/character/create/index.js
@@ -12,14 +12,13 @@ module.exports = {
     },
     chacterIsCreated(stats) {
         return `
-        Ваш интилект ${stats.int}
-        Ваша сила ${stats.str}
-        Ваша телосложение ${stats.con}
-        Ваша сила разума ${stats.men}
-        Ваш локовсть ${stats.dex}
-        Ваше везение ${stats.wit}
+        Ваш интилект ${ stats.int }
+        Ваша сила ${ stats.str }
+        Ваша телосложение ${ stats.con }
+        Ваша сила разума ${ stats.men }
+        Ваш локовсть ${ stats.dex }
+        Ваше везение ${ stats.wit }
        `;
     }
-
 };
 
